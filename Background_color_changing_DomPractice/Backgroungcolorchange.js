ecac3f3d19a299be9da45b902f745924204dc2da
sangram_chavan_patil <100369989+Sangram96k@users.noreply.github.com>
2025-07-20 01:10:31 +0530

    TODO LIST task complete

--- Background_color_changing_DomPractice/Backgroungcolorchange.js
+++ Background_color_changing_DomPractice/Backgroungcolorchange.js
@@ -3,12 +3,16 @@ let color = document.querySelector(".color-code");
 
 toggle.addEventListener('click',()=>{
 let random = getrandom()
-document.body.style.backgroundColor=random;
-color.textContent=random;
+document.body.style.backgroundColor=random;  //.style.backgroundColor accept string value like "red" etc 
+                                             //because of that we has to convert the random  hex code  to string 
+ color.textContent=random;
 
 })
 
 function getrandom(){
     const hex = Math.floor(Math.random() * 0xffffff).toString(16);
-    return `#${hex.padStart(6,"0")}`;
+    return `#${hex.padStart(6,"0")}`; //hex.padStart(6, "0") means:
+                                    //👉 If the string is less than 6 characters,
+                                    //  add "0" to the start until 
+                                    // it becomes 6 characters long.
 }
